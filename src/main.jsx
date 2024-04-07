e4ae8fd211324c0aeefd5078fc3d6fd4eba35e8b
Boluwatife Ade-Ojo <adeojopeter@gmail.com>
2024-04-07 17:36:24 +0100

    installed react-router and tailwind and reduxjs

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+
+import Login from './pages/Login.jsx';
+
+import {
+	createBrowserRouter,
+	RouterProvider,
+	createRoutesFromElements,
+	Route,
+} from 'react-router-dom';
+
+const routes = createBrowserRouter(
+	createRoutesFromElements(
+		<Route path='/'>
+			<Route element={<App />} index />
+			<Route element={<Login />} path='/login' />
+		</Route>
+	)
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<RouterProvider router={routes}></RouterProvider>
+	</React.StrictMode>
+);
