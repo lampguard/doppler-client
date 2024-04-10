7ed3a74a62ad16e6be0d48b02d56dd8a592be32b
Boluwatife Ade-Ojo <adeojopeter@gmail.com>
2024-04-10 13:36:24 +0100

    articulat config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
-    fontFamily: {},
+    fontFamily: {
+      'articulat': ['Articulat', 'ui-sans-serif', 'sans-serif'],
+      'articulat-bold': ['ArticulatBold', 'ui-sans-serif', 'sans-serif'],
+      'articulat-light': ['ArticulatLight', 'ui-sans-serif', 'sans-serif'],
+      'articulat-oblique': ['ArticulatOblique', 'ui-sans-serif', 'sans-serif'],
+    },
   },
   plugins: [require("daisyui")],
   daisyui: {
