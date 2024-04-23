e51ad490ac20a43909119af6e033e0f5109b6586
Boluwatife Ade-Ojo <adeojopeter@gmail.com>
2024-04-23 15:15:00 +0100

    view task

--- src/services/tasks.js
+++ src/services/tasks.js
@@ -1,29 +1,39 @@
-import { api } from '.';
+import { api } from ".";
 
 export const tasks = api
-	.enhanceEndpoints({
-		addTagTypes: ['Tasks', 'Task'],
-	})
-	.injectEndpoints({
-		endpoints: (builder) => ({
-			getTasks: builder.query({
-				query: () => ({
-					url: '/tasks',
-				}),
-				providesTags: ['Tasks'],
-				forceRefetch: ({}) => {
-					return true;
-				},
-			}),
-			assignTask: builder.mutation({
-				query: (body) => ({
-					url: '/tasks',
-					method: 'POST',
-					body,
-				}),
-				invalidatesTags: ['Tasks'],
-			}),
-		}),
-	});
+  .enhanceEndpoints({
+    addTagTypes: ["Tasks", "Task"],
+  })
+  .injectEndpoints({
+    endpoints: (builder) => ({
+      getTasks: builder.query({
+        query: () => ({
+          url: "/tasks",
+        }),
+        providesTags: ["Tasks"],
+        forceRefetch: ({}) => {
+          return true;
+        },
+      }),
+      assignTask: builder.mutation({
+        query: (body) => ({
+          url: "/tasks",
+          method: "POST",
+          body,
+        }),
+        invalidatesTags: ["Tasks"],
+      }),
+      /**
+        @returns {import('@reduxjs/toolkit/query/react').QueryDefinition}
+      */
+      getTask: builder.query({
+        query: (id) => ({
+          url: `/tasks/${id}`,
+        }),
+        providesTags: ["Task"],
+      }),
+    }),
+  });
 
-export const { useLazyGetTasksQuery, useAssignTaskMutation } = tasks;
+export const { useLazyGetTasksQuery, useAssignTaskMutation, useGetTaskQuery } =
+  tasks;
