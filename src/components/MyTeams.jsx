6d21412deee905f328f27657042b257952293b9f
Nkwo Chukwuemeka <emekankwo49@gmail.com>
2024-06-24 17:07:52 +0100

    Implemented Team Modals for Adding App and Adding Member

--- src/components/MyTeams.jsx
+++ src/components/MyTeams.jsx
@@ -1,14 +1,17 @@
-import { useEffect, useState } from 'react';
-import { useLazyGetTeamsQuery } from '../services/teams';
-import { CgMoreVerticalAlt as BsMenuApp } from 'react-icons/cg';
-import { useTeamContext } from '../context/TeamContext.jsx';
-import { Link } from 'react-router-dom';
-import { FaTimes } from 'react-icons/fa';
+import { useEffect, useState } from "react";
+import { useLazyGetTeamsQuery } from "../services/teams";
+import { CgMoreVerticalAlt as BsMenuApp } from "react-icons/cg";
+import { useTeamContext } from "../context/TeamContext.jsx";
+import { useNavigate } from "react-router-dom";
+import { FaTimes } from "react-icons/fa";
+import { Popover } from "antd";
+import AddNewMemberModal from "./MyTeam/AddNewMember.jsx";
+import AddNewAppModal from "./MyTeam/AddNewApp.jsx";
 
 const defaultActiveTeam = {
-	id: undefined,
-	name: 'All Teams',
-	selected: true,
+  id: undefined,
+  name: "All Teams",
+  selected: true,
 };
 
 /**
@@ -16,73 +19,133 @@ const defaultActiveTeam = {
  *
  */
 const MyTeams = ({ setOpen }) => {
-	const [getTeams] = useLazyGetTeamsQuery();
-	const [teams, setTeams] = useState([defaultActiveTeam]);
+  const [getTeams, { isLoading: teamsLoading }] = useLazyGetTeamsQuery();
+  const [teams, setTeams] = useState([defaultActiveTeam]);
+  const navigate = useNavigate();
+  const teamCtx = useTeamContext();
+  const [activeTeam, setActiveTeam] = useState(teamCtx?.value || teams[0]);
+  const popoverLinksStyling = "cursor-pointer hover:underline w-full font-400";
 
-	const teamCtx = useTeamContext();
-	const [activeTeam, setActiveTeam] = useState(teamCtx?.value || teams[0]);
+  useEffect(() => {
+    getTeams()
+      .unwrap()
+      .then((response) => {
+        setTeams([defaultActiveTeam, ...response]);
+        setActiveTeam(teamCtx.value || defaultActiveTeam);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
 
-	useEffect(() => {
-		getTeams()
-			.unwrap()
-			.then((response) => {
-				setTeams([defaultActiveTeam, ...response]);
-				setActiveTeam(teamCtx.value || defaultActiveTeam);
-			})
-			.catch((err) => {
-				console.error(err);
-			});
-	}, []);
+  function selectTeam(id) {
+    const team = teams.find((team) => team.id == id);
+    setTeams(
+      teams.map((team) => {
+        return { ...team, selected: team.id == id };
+      })
+    );
+    setActiveTeam(team);
+    setOpen(false);
+    teamCtx.updateValue(team);
+  }
 
-	function selectTeam(id) {
-		const team = teams.find((team) => team.id == id);
-		setTeams(
-			teams.map((team) => {
-				return { ...team, selected: team.id == id };
-			})
-		);
-		setActiveTeam(team);
-		setOpen(false);
-		teamCtx.updateValue(team);
-	}
+  const [showAddNewMemberModal, setShowAddNewMemberModal] = useState(false);
+  const [showAddNewAppModal, setShowAddNewAppModal] = useState(false);
 
-	return (
-		<div className="bg-white h-full w-full">
-			<div className="flex md:block text-3xl justify-between items-center">
-				<p>My Teams</p>
-				<button className="md:hidden btn btn-ghost" onClick={() => setOpen(false)}>
-					<FaTimes className="text-2xl" />
-				</button>
-			</div>
-			<div className="py-2"></div>
-			{teams.map((team) => {
-				return (
-					<div key={team.name} className="flex items-center justify-between">
-						<label htmlFor="" className="label flex gap-x-1">
-							<input
-								type="radio"
-								name="team"
-								className="radio radio-xs border"
-								checked={team.id == activeTeam.id}
-								onChange={(e) => {
-									selectTeam(team.id);
-								}}
-							/>
-							{team.name}
-						</label>
-						{team.id !== undefined && (
-							<Link
-								to={`/teams/${team.id}`}
-								className="btn btn-sm btn-circle btn-ghost pr-5 sm:pr-0"
-							>
-								<BsMenuApp />
-							</Link>
-						)}
-					</div>
-				);
-			})}
-		</div>
-	);
+  return (
+    <>
+      {showAddNewMemberModal && (
+        <AddNewMemberModal
+          showAddNewModal={showAddNewMemberModal}
+          setShowAddNewModal={setShowAddNewMemberModal}
+        />
+      )}
+      {showAddNewAppModal && (
+        <AddNewAppModal
+          showAddNewModal={showAddNewAppModal}
+          setShowAddNewModal={setShowAddNewAppModal}
+        />
+      )}
+      <div className="bg-white h-full w-full">
+        <div className="flex md:block text-3xl justify-between items-center">
+          <p>My Teams</p>
+          <button
+            className="md:hidden btn btn-ghost"
+            onClick={() => setOpen(false)}
+          >
+            <FaTimes className="text-2xl" />
+          </button>
+        </div>
+        <div className="py-2"></div>
+        <div className="flex flex-col gap-2">
+          {teamsLoading ? (
+            <>
+              <span>Loading...</span>
+            </>
+          ) : (
+            teams?.map((team) => {
+              return (
+                <div
+                  key={team.name}
+                  className="flex items-center justify-between"
+                >
+                  <label htmlFor="" className="label flex gap-x-1">
+                    <input
+                      type="radio"
+                      name="team"
+                      className="radio radio-xs border"
+                      checked={team?.id == activeTeam.id}
+                      onChange={(e) => {
+                        selectTeam(team.id);
+                      }}
+                    />
+                    {team?.name}
+                  </label>
+                  {team.id !== undefined && (
+                    <Popover
+                      placement="bottomRight"
+                      className="]"
+                      arrow={false}
+                      content={
+                        <div className="w-[200px] flex flex-col gap-5">
+                          <p
+                            onClick={() => navigate(`/teams/${team.id}`)}
+                            className={popoverLinksStyling}
+                          >
+                            Team Info
+                          </p>
+                          <p
+                            className={popoverLinksStyling}
+                            onClick={() => setShowAddNewAppModal(true)}
+                          >
+                            Add App
+                          </p>
+                          <p
+                            className={popoverLinksStyling}
+                            onClick={() => setShowAddNewMemberModal(true)}
+                          >
+                            Add Members
+                          </p>
+                          <p className={popoverLinksStyling}>Rename Team</p>
+                          <p className={popoverLinksStyling}>Dissolve Team</p>
+                        </div>
+                      }
+                      trigger="hover"
+                    >
+                      <span className="btn btn-sm btn-circle btn-ghost pr-5 sm:pr-0 cursor-pointer">
+                        <BsMenuApp />
+                      </span>
+                    </Popover>
+                  )}
+                </div>
+              );
+            })
+          )}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default MyTeams;
